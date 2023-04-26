d8d9f9a2def9d61086a547c77c4b097c76f07335
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-04-26 18:16:10 +0200

    Changed Reward Datatype ZonedDateTime to LocalDateTime

--- backend/src/main/java/de/neuefische/backend/reward/Reward.java
+++ backend/src/main/java/de/neuefische/backend/reward/Reward.java
@@ -2,16 +2,16 @@ package de.neuefische.backend.reward;
 
 import org.springframework.data.annotation.Id;
 
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 
 public record Reward(
         @Id
-        String rewardID,
+        String rewardId,
         String name,
         String description,
         double price,
         double savingAllocated,
         boolean statusOpen,
-        ZonedDateTime rewardCreated
+        LocalDateTime rewardCreated
 ) {
 }
