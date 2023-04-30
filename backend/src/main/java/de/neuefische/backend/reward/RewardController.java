df818b6da43d86cafa87387075c71b0cf27bc212
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-04-30 02:39:10 +0200

    Post-endpoint with addReward method in backend added

--- backend/src/main/java/de/neuefische/backend/reward/RewardController.java
+++ backend/src/main/java/de/neuefische/backend/reward/RewardController.java
@@ -1,9 +1,7 @@
 package de.neuefische.backend.reward;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +15,7 @@ public class RewardController {
     @GetMapping
     public List<Reward> getAll() {return rewardService.getAll();}
 
+    @PostMapping
+    public Reward addReward(@RequestBody Reward reward){return rewardService.addReward(reward);}
 
 }
