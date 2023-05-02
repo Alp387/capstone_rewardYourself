036eeb6fca9e8e9d7e716687995e81643515a1de
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-05-02 02:57:43 +0200

    function loadAllRewards renamed to getAllRewards; New Reward is not in header anymore. Its in same div like RewardGallery

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,67 @@
 import React, {useEffect, useState} from 'react';
-
 import './App.css';
-import RewardGallery from "./RewardGallery";
-import {NewReward, Reward} from "./Reward";
-import axios, {AxiosResponse} from "axios";
-import MainBar from "./MainBar";
-import AddReward from "./AddReward"
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Button} from "@mui/material";
+import RewardGallery from './RewardGallery';
+import {NewReward, Reward} from './Reward';
+import axios, {AxiosResponse} from 'axios';
+import MainBar from './MainBar';
+import AddReward from './AddReward';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Button} from '@mui/material';
 
 function App() {
-    const [rewards, setRewards] = useState<Reward[]>([])
+    const [rewards, setRewards] = useState<Reward[]>([]);
 
     useEffect(() => {
-        loadAllRewards()
-    }, [])
+        getAllRewards();
+    }, []);
 
-    function loadAllRewards() {
-        axios.get("/api/rewards")
+    function getAllRewards() {
+        axios
+            .get('/api/rewards')
             .then((response: AxiosResponse<any>) => {
-                setRewards(response.data)
+                setRewards(response.data);
             })
             .catch((error) => {
-                console.error(error)
-            })
+                console.error(error);
+            });
     }
 
     function addReward(newReward: NewReward) {
-        axios.post("/api/rewards/add", newReward)
-            .then(() => loadAllRewards())
-            .catch(() => console.error("post on /api/rewards/add not successful"))
+        axios
+            .post('/api/rewards/add', newReward)
+            .then(() => getAllRewards())
+            .catch(() => console.error('post on /api/rewards/add not successful'));
     }
 
     return (
         <BrowserRouter>
             <div className="App">
-                <header className="App-header">
-                    <MainBar/>
-                    <Button size="small" variant="contained" href="/api/rewards/add">
-                        New Reward
-                    </Button>
-
-                </header>
-                <Routes>
-                    <Route path="/"
-                           element={<RewardGallery rewards={rewards}/>}/>
-                    <Route path="/api/rewards/add"
-                           element={<AddReward addReward={addReward}/>}/>
-                </Routes>
+                <MainBar/>
+                <div className="content">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <>
+                                    <div className="button-container">
+                                        <Button
+                                            size="small"
+                                            variant="contained"
+                                            href="/api/rewards/add"
+                                        >
+                                            New Reward
+                                        </Button>
+                                    </div>
+                                    <RewardGallery rewards={rewards}/>
+                                </>
+                            }
+                        />
+                        <Route
+                            path="/api/rewards/add"
+                            element={<AddReward addReward={addReward}/>}
+                        />
+                    </Routes>
+                </div>
             </div>
         </BrowserRouter>
     );
