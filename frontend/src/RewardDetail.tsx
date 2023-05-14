43021b16b563ebe3bfdc1ebf8703d6e0f2e8ca5d
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-05-14 23:14:25 +0200

    delete button implemented

--- frontend/src/RewardDetail.tsx
+++ frontend/src/RewardDetail.tsx
@@ -23,14 +23,26 @@ export default function RewardDetail() {
             console.log("Reward not found " + error);
         })
     }
+
+    function deleteReward(id: string) {
+        axios.delete('/api/rewards/' + id).catch((error) => {
+            console.log("Failed to delete reward" + error)
+        })
+        navigate('/rewards/')
+    }
+
     return (
         <div>
             {reward ? (
-                <div><Button size="small" onClick={() => {
-                    navigate('/rewards/' + reward.id + '/update', {state: reward})
-                }}>
-                    Update Reward
-                </Button>
+                <div>
+                    <Button size="small" onClick={() => {
+                        navigate('/rewards/' + reward.id + '/update', {state: reward})
+                    }}>
+                        Update Reward
+                    </Button>
+                    <Button size="small" onClick={() => deleteReward(reward.id)}>
+                        Delete Reward
+                    </Button>
                     <h1>Name: {reward.name}</h1>
                     <p>Description: {reward.description}</p>
                     <p>Price: {reward.price}</p>
