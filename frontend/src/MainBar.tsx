fceb3286dbdc025170afb20b8648014a43dcc11e
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-05-22 23:41:07 +0200

    user in frontend implemented

--- frontend/src/MainBar.tsx
+++ frontend/src/MainBar.tsx
@@ -1,6 +1,26 @@
 import {Button, Stack} from "@mui/material";
+import {useEffect, useState} from "react";
+import {User} from "./User";
+import axios from "axios";
 
 export default function MainBar() {
+    const [user, setUser] = useState<User>(
+        {id: "", name: "", totalSaving: -1});
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
+    function getUser() {
+        axios.get('/api/user')
+            .then((response) => {
+                setUser(response.data);
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
 
     return (
         <div className="MainBar">
@@ -8,6 +28,10 @@ export default function MainBar() {
                 <Button size="small" variant="contained" href="/">
                     Home
                 </Button>
+                {user && (
+                    <p>
+                        {user.name} - Total Saving: {user.totalSaving}</p>
+                )}
             </Stack>
         </div>
 
