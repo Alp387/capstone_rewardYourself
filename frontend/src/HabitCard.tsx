eb4e198fe20e678e0031c26dfd064a705c48f039
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-05-23 13:37:30 +0200

    HabitCard displays how many hours are passed since habit was created

--- frontend/src/HabitCard.tsx
+++ frontend/src/HabitCard.tsx
@@ -3,22 +3,32 @@ import {Button} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
 type HabitCardProps = {
-    habit: Habit
-}
+    habit: Habit;
+};
+
 export default function HabitCard(props: HabitCardProps) {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
+    function calculateTimeDifference(oldTimestamp: Date) {
+                return Math.round((Date.now() - Date.parse(oldTimestamp.toString()))/(1000 * 60 * 60))
+    }
 
     return (
         <div className="habit-card">
-            <p>Name: {props.habit.name};
+            <p>
+                Name: {props.habit.name};
                 Daily saving: {props.habit.dailySaving};
-                Last collection x hours ago
-                <Button size="small" variant="text" onClick={() => {
-                    navigate('/habits/' + props.habit.id)
-                }}>
+                Active since: {calculateTimeDifference(props.habit.startTime)} hours
+                <Button
+                    size="small"
+                    variant="text"
+                    onClick={() => {
+                        navigate('/habits/' + props.habit.id);
+                    }}
+                >
                     Details
                 </Button>
             </p>
         </div>
-    )
+    );
 }
